8c67286adc57f29fcca2b3248b616b523328f9db
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-01-18 00:27:38 +0100

    Voice Channel User Detection

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+import discord
 from discord.ext import commands
 
 from src.utils import *
@@ -5,11 +8,27 @@ from src.utils import *
 
 class CyberPYG(commands.Bot):
     def __init__(self, prefix: str):
+        """ Bot's constructor """
+
         super().__init__(command_prefix=prefix)
+        self.bot_instance = self.user
 
     @commands.Cog.listener()
     async def on_ready(self):
+        """ Bot's ready """
+
         print(f"Connecté en tant que {self.user}")
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: Union[discord.User, discord.Member], before, after):
+        """ Voice User State Update """
+
+        if "Salon de" in after.channel.name:
+            # When an user joins/creates a private channel
+
+            print("F")
+
+
 
 
 if __name__ == '__main__':
