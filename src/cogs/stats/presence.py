83a84e74e2f960c4ca47e5965615c934f7a4542c
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-01-18 01:13:03 +0100

    bot author verfication, activty cmd

--- src/cogs/stats/presence.py
+++ src/cogs/stats/presence.py
@@ -18,27 +18,29 @@ class StatisticsPresence(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.activity_channels: dict = {}
-        self.user_activity: dict = {}
+        self.users_activity: dict = {}
 
     @commands.command(name='activty')
     async def activity(self, ctx):
         await safe_delete(ctx)
+        for user in self.users_activity:
+            await ctx.channel.send(f"L'utilisateur {user.name} avec {self.users_activity[user]} messages.")
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         """ When a message is found by the API """
 
-        if message.author is not message.author.bot:  # If the message's not comming from a bot
+        if not message.author.bot:  # If the message's not comming from a bot
             self.increment_user_amount(message.author)
-            print(f"{self.user_activity}")
+            print(f"{self.users_activity}")
 
     def increment_user_amount(self, target: Union[discord.Member, discord.User]):
         """ User message amount handler"""
 
-        if target not in self.user_activity:
-            self.user_activity[target] = 1
+        if target not in self.users_activity:
+            self.users_activity[target] = 1
         else:
-            self.user_activity[target] += 1
+            self.users_activity[target] += 1
 
 
 def setup(bot):
