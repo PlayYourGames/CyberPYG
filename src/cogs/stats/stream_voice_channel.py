ae976c9b6955b29c28a4657c16979cc04c4b9202
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-01-24 13:37:14 +0100

    [unstable] StreamVC implementation

--- src/cogs/stats/stream_voice_channel.py
+++ src/cogs/stats/stream_voice_channel.py
@@ -12,10 +12,16 @@ class Stream_VC(commands.Cog):
     async def on_voice_state_update(self, member: discord.Member, before, after):
         """ Voice User State Update """
 
+        user_channel = member.voice.channel if member.voice else False
+        old_name: str = after.channel.name
+        print(f"Old name = {old_name}")
+
         if before.channel and after.channel:  # User's connected before and after the update
-            if before.self_stream != after.self_stream:  # If user's currently streaming
+            print(f"Stream : {after.self_stream}")
+
+            if after.self_stream:  # If user's currently streaming
                 if "Salon de" in after.channel.name:  # If the channel's a private one
-                    user_channel = member.voice.channel
+                    old_name = user_channel.name
                     game_activity = None
 
                     """ Check throughout the different activities the user may have """
@@ -23,7 +29,17 @@ class Stream_VC(commands.Cog):
                         if member.activities[i].type.__eq__(discord.ActivityType.playing):
                             game_activity = member.activities[i]
 
+                    print(game_activity.name)
+                    # todo: Le print d'au-dessus fonctionne, mais le user channel ne semble pas éditer, ou une fois sur deux, protection de l'API ?
                     await user_channel.edit(name=f"{game_activity.name} - {member.name}")
+
+            else:  # User stop streaming [stable]
+                print(2)
+                await user_channel.edit(name=old_name)
+        elif not user_channel:  # User leaves channel
+            # todo: L'user channel est déjà null, l'événement trigger après que le membre quitte, il faudrait pouvoir récupérer le channel via le bot guild
+            print(3)
+            # await user_channel.edit(name=old_name)
 
 
 def setup(bot):
